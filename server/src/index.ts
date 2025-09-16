d4a28e672c200edd6bd15fe544798e30bea51c34
ishaansaxena25 <saxenaishaan2004@gmail.com>
2025-09-16 17:03:21 +0530

    fixed api

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,29 @@
-require("dotenv").config({path:"./.env"});
-import express, {Request, Response} from "express"
-import {startServer, app} from './util'
-import {trackBus, getNearestBusStops, getNextStop, getBusesForStop, getCommonRoutes} from "./controller/busController"
+require("dotenv").config({ path: "./.env" });
+import express, { Request, Response } from "express";
+import { startServer, app } from "./util";
+import {
+  trackBus,
+  getNearestBusStops,
+  getNextStop,
+  getBusesForStop,
+  getCommonRoutes,
+} from "./controller/busController";
 import { rateLimiter } from "./middleware/rateLimiter";
 
 //middlewares
 app.use(express.json());
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(rateLimiter);
 
 //APIs
-app.get("/test", (req:Request ,res:Response)=>{res.json("server running");})
-app.get("/getNearestBustops", getNearestBusStops)
-app.get("/getNextStop", getNextStop)
-app.get("/getBusesForStop", getBusesForStop);   
-app.get("/getCommonRoutes", getCommonRoutes);
+app.get("/test", (req: Request, res: Response) => {
+  res.json("server running");
+});
+app.post("/getNearestBustops", getNearestBusStops);
+app.post("/getNextStop", getNextStop);
+app.post("/getBusesForStop", getBusesForStop);
+app.post("/getCommonRoutes", getCommonRoutes);
 
-app.post("/trackBus", trackBus)
+app.post("/trackBus", trackBus);
 
 startServer();
